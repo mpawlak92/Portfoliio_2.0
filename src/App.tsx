71f0b5f50364c0003641220f948d09c2d58837dd
mpawlak92 <mpawlak.it@gmail.com>
2024-02-06 17:37:17 +0100

    config for deploy on github pages with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ThemeProvider, styled } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import { light, dark } from './theme';
 import { ThemeContext } from './stores/context';
@@ -17,20 +18,22 @@ function App() {
   };
   return (
     <>
-      <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
-        <ThemeProvider theme={theme === 'light' ? light : dark}>
-          <GlobalStyle />
-          <Container>
-            <h1>Vite + React</h1>
-            <div className="card">
-              <button onClick={changeTheme}>theme is {theme}</button>
-              <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-              </p>
-            </div>
-          </Container>
-        </ThemeProvider>
-      </ThemeContext.Provider>
+      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Portfolio_2.0'}>
+        <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
+          <ThemeProvider theme={theme === 'light' ? light : dark}>
+            <GlobalStyle />
+            <Container>
+              <h1>Vite + React</h1>
+              <div className="card">
+                <button onClick={changeTheme}>theme is {theme}</button>
+                <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+              </div>
+            </Container>
+          </ThemeProvider>
+        </ThemeContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
