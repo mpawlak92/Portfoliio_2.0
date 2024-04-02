44056aa345ad2f85ab7102c4e9afc8a23b5b861e
mpawlak92 <mpawlak.it@gmail.com>
2024-04-02 14:30:07 +0200

    added cta colors and shades to theme

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,6 @@
 import 'styled-components';
 interface Palette {
-  main: string;
+  
   text: string;
   background: string;
 }
@@ -21,9 +21,37 @@ declare module 'styled-components' {
           200: string;
           100: string;
         },
-        white: string;
-        primary: string;
-        secondary: string;
+        white:{
+          1000: string;
+          900: string;
+          800: string;
+          700: string;
+          600: string;
+          500: string;
+          400: string;
+          300: string;
+          200: string;
+          100: string;
+        },
+        cta:{
+          1000: string;
+          750: string;
+          500: string;
+          250: string;
+          0: string;
+         
+        },
+        ctaShades:{
+          900: string;
+          800: string;
+          700: string;
+          600: string;
+          500: string;
+          400: string;
+          300: string;
+          200: string;
+          100: string;
+        },
       };
       primary: Palette;
       secondary: Palette;
