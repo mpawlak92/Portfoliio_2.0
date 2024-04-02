d91a1403f76a244887aac7fdda8a6f7b6b383bf2
mpawlak92 <mpawlak.it@gmail.com>
2024-04-02 14:30:22 +0200

    added biger brakepoints

--- src/style/Brakepoints.ts
+++ src/style/Brakepoints.ts
@@ -5,5 +5,9 @@ export const device = {
   laptop: `(min-width: 1200px)`,
   laptopL: `(min-width: 1400px)`,
   desktop: `(min-width: 1600px)`,
-  desktopL: `(min-width: 2500px)`,
+  fullHD: `(min-width: 1980px)`,
+  '2500px': `(min-width: 2500px)`,
+  '3000px': `(min-width: 3000px)`,
+  '4K': `(min-width: 38400px)`,
+  
 };
