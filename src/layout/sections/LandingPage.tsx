50b993965540f6e3a2413cdfde6bff872b0845a7
mpawlak92 <mpawlak.it@gmail.com>
2024-04-02 14:31:59 +0200

    created portfolio button and added style for responsive behavior

--- src/layout/sections/LandingPage.tsx
+++ src/layout/sections/LandingPage.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
 import { keyframes, styled } from 'styled-components';
 import { ThemeContext } from '../../stores/theme-context';
+import { Link } from 'react-router-dom';
 
 import heroBgLight from '../../assets/hero_bg_light.jpg';
 import heroBgDark from '../../assets/hero_bg_dark.jpg';
-import { Link } from 'react-router-dom';
+import {device} from '../../style/Brakepoints'
 
 
 
@@ -60,9 +61,10 @@ const Container = styled.div<ContainerProps>`
   background-repeat: no-repeat;
   background-position: center;
   background-clip: border-box;
-  font-size: 10em;
+  font-size: 1rem;
   z-index:5;
   overflow: hidden;
+  
 `;
 const Overlay = styled.div`
   position: absolute;
@@ -70,7 +72,7 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.theme.palette.common.black[600]};
+  background-color: ${(props) => props.theme.palette.common.black[400]};
   z-index:10;
 `
 const fadeIn = keyframes`
@@ -90,8 +92,9 @@ const Heading = styled.header<HeadingProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 2em;
-  color: ${props=> props.theme.palette.common.white};
+  font-size: 5em;
+  color: ${props=> props.theme.palette.common.white[800]};
+  text-shadow: 5px 5px 2px ${props=> props.theme.palette.common.black[500]};;
   text-align: center;
   z-index:11;
   transition: opacity .5s linear;
@@ -100,17 +103,44 @@ const Heading = styled.header<HeadingProps>`
     animation: ${fadeIn} .5s linear forwards;
     animation-delay: 0.5s;
   }
+
+  @media (${device.mobileM}) {
+    font-size: 7em;
+  }
+  @media (${device.tablet}) {
+    font-size: 7.2em;
+  }
+  @media (${device.laptopL}) {
+    font-size: 8em;
+  }
 `
 const PortfolioBtn = styled(Link)`
   margin-block-start: 20px ;
-  padding-inline: 65px;
-  padding-block: 25px;
-
-  font-size: 25px;
-  color: ${(props) => props.theme.palette.common.white};
-  background-color: ${(props) => props.theme.palette.secondary.background};
+  padding-inline: 20px;
+  padding-block: 15px;
+  aspect-ratio: 3/1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.4em;
+  color: ${(props) => props.theme.palette.common.white[900]};
+  background-color: ${(props) => props.theme.palette.common.cta[500]};
   text-decoration: none;
   border-radius: 8px;
+  box-shadow: 0px 5px 5px ${(props) => props.theme.palette.common.black[400]};
   z-index:11;
+  &:hover{
+    background-color: ${(props) => props.theme.palette.common.cta[750]};
+  }
+  @media (${device.mobileM}) {
+    font-size: 3em;
+  }
+  @media (${device.tablet}) {
+    font-size: 3.2em;
+  }
+  
+  @media (${device.laptopL}) {
+    font-size: 3.8em;
+  }
 `;
 export default LandingPage;
